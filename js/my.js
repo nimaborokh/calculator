2e56e5d36b6e275861632b4e18fd3387ffb98fd9
nimaborokh <nimaborokh@gmail.com>
2023-10-07 22:05:45 +0330

    .

--- js/my.js
+++ js/my.js
@@ -3,6 +3,10 @@ function buttonsValue(a) {
     return (p.value += a);
 }
 function cleard() {
-    p.value= "";
+    p.value = "";
 }
-
+function inputValue(id) {
+    let element = document.getElementById(id);
+    let value = element.value;
+    console.log(value);
+}
